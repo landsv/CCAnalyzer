d83f5491ddaf9633142bcbb9a4dc6b070f2e14a9
landsv <belmeha@gmail.com>
2017-08-11 22:23:22 +0300

    Added test target. Added test for create request method

---
+++ Tests/CCAnalyzerTests/CCAnalyzerTests.swift
@@ -0,0 +1,17 @@
+import Foundation
+import XCTest
+import CCAnalyzerCore
+
+class CCAnalyzerTests: XCTestCase {
+    
+    func testCreateGetRequest() throws {
+        let httpClient = HTTPClient(baseURL: URL(string: "https://google.com")!)
+        let rq = httpClient.createRequest(withMethod: .GET, path: "/auth/login", parameters: ["q":"query"])
+        
+        XCTAssertEqual(rq?.url?.scheme, "https")
+        XCTAssertEqual(rq?.url?.host, "google.com")
+        XCTAssertEqual(rq?.url?.path, "/auth/login")
+        XCTAssertEqual(rq?.url?.query, "q=query")
+    }
+    
+}
