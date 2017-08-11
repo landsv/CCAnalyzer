d7f69c485b4282a48e6f8c53831f81d9e6b4c180
landsv <belmeha@gmail.com>
2017-08-11 20:32:06 +0300

    Init commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CCAnalyzer",
+    dependencies: [
+        .package(url: "https://github.com/Swinject/Swinject", .branch("swift4")),
+        .package(url: "https://github.com/kylef/Commander", from: "0.6.0"),
+        .package(url: "https://github.com/onevcat/Rainbow", from: "2.1.0")
+    ],
+    targets: [
+        .target(
+            name: "CCAnalyzer",
+            dependencies: ["CCAnalyzerCore", "Rainbow"]
+        ),
+        .target(
+            name: "CCAnalyzerCore",
+            dependencies: ["Commander", "Swinject"]
+        )
+    ]
+)
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# CCAnalyzer
+
+A description of this package.
